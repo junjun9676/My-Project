9776d7943733b9e70334263882dbba73c4f41f6a
junjun9676 <51070316+junjun9676@users.noreply.github.com>
2025-03-11 22:55:47 +0800

    Add files via upload

---
+++ springboot-image-tool/src/test/java/com/jun/springbootimagetool/SpringbootImageToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun.springbootimagetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootImageToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
